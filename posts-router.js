9b12f3d1aa2a0359966f362439a1a63e3dd18586
Sem8 <mssemmi8@gmail.com>
2019-03-19 20:02:43 -0400

    delete request added

--- posts-router.js
+++ posts-router.js
@@ -89,5 +89,20 @@ router.post('/', (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Posts.remove(req.params.id);
+        if (count > 0) { 
+            res.status(200).end();            
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "The post could not be removed" })
+    }  
+
+})
+
 
 module.exports = router;
