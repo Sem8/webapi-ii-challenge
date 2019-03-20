e9786870e014307dd657bbf6abe47b6c725d1e2e
Sem8 <mssemmi8@gmail.com>
2019-03-20 00:33:12 -0400

    post content rendered onto react app

--- posts-api-react/src/App.js
+++ posts-api-react/src/App.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-
   state = {
     posts: []
   };
 
   componentDidMount() {
     this.getPosts();
-   
-  };
+  }
 
   getPosts = () => {
-    fetch('http://localhost:4000/api/posts')
-    .then(res => res.json())
-    .then(res => this.setState({ posts: res }));
+    fetch("http://localhost:4000/api/posts")
+      .then(res => res.json())
+      .then(res => this.setState({ posts: res }));
   };
 
-  renderPost = ({ id, title, contents }) => <div key={id}>{title} - {contents}</div>
+  renderPost = ({ id, title, contents }) => (
+    <div key={id}>
+    <h1>{title}</h1>    
+    <p>{contents}</p> 
+    - - - - - -       
+    </div>
+  );
   render() {
-    const { posts } = this.state
+    const { posts } = this.state;
     return (
       <div className="App">
-      sanity
-      {posts.map(this.renderPost)}
-
-       
+        sanity
+        {posts.map(this.renderPost)}
       </div>
     );
   }
